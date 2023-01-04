941357c580868b5647eebb8c38aa2cc8b526ccca
Levi Nunnink <levi@culturezoo.com>
2023-01-04 15:19:17 -0500

    fix issue with incorrect endpoints

--- src/Client.ts
+++ src/Client.ts
@@ -1,6 +1,8 @@
 import axios, {AxiosInstance} from 'axios';
 import { Endpoint } from './Endpoint';
 import Metrics from './Metrics';
+import People from './People';
+import Companies from './Companies';
 
 export default class Client {
 	client: AxiosInstance;
@@ -16,7 +18,7 @@ export default class Client {
 		this.client.defaults.headers.common.accept = 'application/json';
 		this.client.defaults.headers.common.Authorization = `Bearer ${this.apiKey}`;
 		this.metrics = new Metrics(this.client, '/metrics');
-		this.people = new Metrics(this.client, '/people');
-		this.companies = new Metrics(this.client, '/people');
+		this.people = new People(this.client, '/people');
+		this.companies = new Companies(this.client, '/companies');
 	}
 }
